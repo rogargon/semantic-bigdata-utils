07d5ec42c90055773f682d4fd22ffa074a0072c4
Roberto Garcia <roberto@rhizomik.net>
2015-07-14 17:02:41 -0400

    Initial commit adds parallelized tool to convert RDF NQuads to NTriples

---
+++ quads-to-triples.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+usage="
+        Parallelized RDF NQuads to NTriples conversion script using GNU Parallel. 
+        Contact: http://rhizomik.net/~roberto/
+        
+        Usage: 
+                $(basename "$0") filename.[nq|nquads]
+"
+
+filename="${1%.*}"
+extension="${1##*.}"
+regex="(.*[^ ])[ ]+<[^ ]+>.*"
+
+if [ $# = 1 ]
+then
+        if [ -f "$1" ]
+        then
+                if [ $extension = "nq" ] || [ $extension = "nquads" ]
+                then
+                        cat $1 | parallel --pipe sed -rn "'s/"$regex"/\1 ./p'" > $filename.nt
+                else
+                        echo "$usage"
+                        echo "  Error: input file extension should be '.nq' or '.nquads'"
+                        echo ""
+                fi
+        else
+                echo "$usage"
+                echo "  Error: file '$1' not found."
+                echo ""
+        fi
+else
+        echo "$usage"
+fi
