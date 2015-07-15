9508d3af8b0564c3402bc77edbf66ff1063a9217
Roberto Garcia <roberto@rhizomik.net>
2015-07-15 14:32:50 -0400

    Added parallelized script linecount.sh that counts file lines

---
+++ linecount.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+usage="
+        Parallelized line count script using GNU Parallel. 
+        Contact: http://rhizomik.net/~roberto/
+        
+        Usage: 
+                $(basename "$0") filename
+"
+
+filename="${1%.*}"
+extension="${1##*.}"
+regex="(.*[^ ])[ ]+<[^ ]+>.*"
+size="--block-size 10M"
+
+if [ $# = 1 ]
+then
+        if [ -f "$1" ]
+        then
+                cat $1 | parallel  --pipe $size wc -l | awk '{s+=$1} END {print s}'
+        else
+                echo "$usage"
+                echo "  Error: file '$1' not found."
+                echo ""
+        fi
+else
+        echo "$usage"
+fi
